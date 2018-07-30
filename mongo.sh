dc68ef151582aaf8d4fab0754eda5b2ade21a9c3
anotheroctopus <li1735@purdue.edu>
2018-07-30 09:08:52 -0700

    adding mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Initialize a mongo data folder and logfile
+mkdir -p /data/db
+touch /var/log/mongodb.log
+
+# Start mongodb with logging
+# --logpath    Without this mongod will output all log information to the standard output.
+# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
+/usr/bin/mongod  --quiet --logpath /var/log/mongodb.log --logappend &
+
+# Wait until mongo logs that it's ready (or timeout after 60s)
+COUNTER=0
+grep -q 'waiting for connections on port' /var/log/mongodb.log
+while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+    grep -q 'waiting for connections on port' /var/log/mongodb.log
+done
+
+# Now we know mongo is ready and can continue with other commands
+mongorestore dump
