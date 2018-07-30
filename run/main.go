57abc68de6eb9c2ff82bd8be354a0f04c2b2934f
anotheroctopus <li1735@purdue.edu>
2018-07-30 10:47:37 -0700

    blah

--- run/main.go
+++ run/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"github.com/AnotherOctopus/goin/cnet"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/AnotherOctopus/goin/cnet"
 )
-func main(){
-	hostdomain,err := os.Hostname()
+
+func exposefiles() {
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/", fs)
+	http.ListenAndServe(":80", nil)
+}
+
+func main() {
+	exposefiles()
+	hostdomain, err := os.Hostname()
 	if err != nil {
 		log.Println("Something up nigga")
 		os.Exit(1)
 	}
 	peerips := []string{}
 	nd := cnet.New(peerips)
-	nd.RequestToJoin(hostdomain,os.Getenv("NETNODE"),os.Getenv("NETNODE")=="")
+	nd.RequestToJoin(hostdomain, os.Getenv("NETNODE"), os.Getenv("NETNODE") == "")
 	go nd.TxListener()
 	go nd.BlListener()
 	go nd.CmdListener()
--- tests/test1.py
+++ tests/test1.py
@@ -3,6 +3,6 @@ NETDIR = "networkfiles"
 print("Loading Wallets")
 goin.loadWal("genesisWallet","172.18.0.2")
 for i in range(3,9):
-    print("Wallet")
     goin.makeWal("newwallet{}".format(i),"172.18.0.{}".format(i))
     goin.loadWal("newwallet{}".format(i),"172.18.0.{}".format(i))
+goin.prepTx()
