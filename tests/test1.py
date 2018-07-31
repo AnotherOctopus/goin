ad5c00467b432350909549668f07f990440335fa
anotheroctopus <li1735@purdue.edu>
2018-07-31 14:40:26 -0700

    loading wallet in jenkinsfile

--- tests/test1.py
+++ tests/test1.py
@@ -1,8 +1,11 @@
 import goin
+import requests
 NETDIR = "networkfiles"
 print("Loading Wallets")
 goin.loadWal("genesisWallet","172.18.0.2")
 for i in range(3,9):
     goin.makeWal("newwallet{}".format(i),"172.18.0.{}".format(i))
+    wallet = requests.get("172.18.0.{}".format(i)) 
+    print(wallet)
     goin.loadWal("newwallet{}".format(i),"172.18.0.{}".format(i))
 goin.prepTx()
