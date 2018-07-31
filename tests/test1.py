7a288507256dcd105f99032741a92be62ff414b6
anotheroctopus <li1735@purdue.edu>
2018-07-31 15:06:22 -0700

    added requests squemea

--- tests/test1.py
+++ tests/test1.py
@@ -5,7 +5,7 @@ print("Loading Wallets")
 goin.loadWal("genesisWallet","172.18.0.2")
 for i in range(3,9):
     goin.makeWal("newwallet{}".format(i),"172.18.0.{}".format(i))
-    wallet = requests.get("172.18.0.{}".format(i)) 
+    wallet = requests.get("http://172.18.0.{}".format(i)) 
     print(wallet)
     goin.loadWal("newwallet{}".format(i),"172.18.0.{}".format(i))
 goin.prepTx()
